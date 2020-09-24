2a78f96615692c184fd2558a304b048609255557
Sam Popov <sam.vpd@gmail.com>
2020-09-24 22:28:55 +0700

    feat(24.09.2020) drop down menu

--- src/modules/dropMenu.js
+++ src/modules/dropMenu.js
@@ -1,5 +1,24 @@
 const dropMenu = () => {
-
+  const clubsList = document.querySelector(".clubs-list"),
+    headerMain = document.querySelector(".header-main"),
+    ul = clubsList.querySelector("ul");
+  ul.style.display = "none";
+  const handlerMenu = () => {
+    headerMain.addEventListener("click", event => {
+      let target = event.target;
+      if (target.closest(".club-select") && ul.style.display === "none") {
+        ul.style.display = "block";
+        return;
+      } else {
+        ul.style.display = "none";
+        target = target.closest(".club-select");
+      }
+      if (!target) {
+        ul.style.display = "none";
+      }
+    });
+  };
+  handlerMenu();
 };
 
 export default dropMenu;
