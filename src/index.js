ff2d870c654a83ec7e32c6455f7e6b54da007289
Sam Popov <sam.vpd@gmail.com>
2020-09-24 23:33:26 +0700

    feat(24.09.2020) text modal

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import elementClosest from "element-closest";
 elementClosest(window);
 
 import dropMenu from "./modules/dropMenu";
+import textModal from "./modules/textModal";
 
 dropMenu();
+textModal();
---
+++ src/modules/textModal.js
@@ -0,0 +1,27 @@
+const textModal = () => {
+  const headerMain = document.querySelector(".header-main"),
+    popup = document.querySelector("#free_visit_form");
+
+  const handlerModal = () => {
+    headerMain.addEventListener("click", event => {
+      const target = event.target;
+      if (target.closest(".free-visit")) {
+        popup.style.display = "block";
+      }
+    });
+    popup.addEventListener("click", () => {
+      let target = event.target;
+      if (target.classList.contains("close-icon")) {
+        popup.style.display = "none";
+      } else {
+        target = target.closest(".form-content");
+      }
+      if (!target) {
+        popup.style.display = "none";
+      }
+    });
+  };
+  handlerModal();
+};
+
+export default textModal;
