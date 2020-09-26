0f8c6567b0fe06b65baa7c7cd1a17cfdce72dd41
Sam Popov <sam.vpd@gmail.com>
2020-09-27 05:34:59 +0700

    feat(27.09.2020) fix send form

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -4,8 +4,6 @@ const sendForm = () => {
     loadMessage = "Загрузка...",
     checkMessage = "Необходимо заполнить все поля!";
 
-
-
   const form = document.querySelectorAll("form"),
     statusMessage = document.createElement("div"),
     popUpMessage = document.querySelector("#thanks"),
@@ -34,16 +32,6 @@ const sendForm = () => {
     });
   };
   validatorInputs();
-
-  const clearInput = () => {
-    const input = document.querySelectorAll('input');
-
-    input.forEach(item => {
-      if (item.type === 'text' || item.type === 'tel') {
-        item.value = '';
-      }
-    });
-  };
 
   const checkedClubs = (mozClub, schelClub) => {
 
@@ -76,13 +64,17 @@ const sendForm = () => {
 
   form.forEach(item => {
     const checkBox = item.querySelector("input[type=checkbox]"),
-      inputsForm = item.querySelectorAll('input');
+      inputsForm = item.querySelectorAll('input'),
+      inputText = item.querySelector('input[type="text"]'),
+      inputTel = item.querySelector('input[type="tel"]');
+
     inputsForm.forEach(item => {
       if (item.hasAttribute('required')) {
         item.removeAttribute('required');
         item.style.outline = 'none';
       }
     });
+
     item.addEventListener("submit", event => {
       event.preventDefault();
       item.append(statusMessage);
@@ -93,6 +85,21 @@ const sendForm = () => {
         body[key] = val;
       });
 
+      if ((inputText && inputText.value === '') || inputTel.value === '') {
+        p.textContent = checkMessage;
+        h4.textContent = "Oops!";
+        popUpMessage.style.display = "block";
+        setTimeout(() => {
+          statusMessage.textContent = '';
+          popUpMessage.style.display = "none";
+        }, 5000);
+        return;
+      } else {
+        p.textContent = "Ваша заявка отправлена.Мы свяжемся с вами в ближайшее время.";
+        h4.textContent = "Спасибо!";
+        popUpMessage.style.display = "block";
+      }
+
       if (checkBox && !checkBox.checked) {
         p.textContent = checkMessage;
         h4.textContent = "Oops!";
@@ -102,13 +109,17 @@ const sendForm = () => {
           popUpMessage.style.display = "none";
         }, 5000);
         return;
+      } else {
+        p.textContent = "Ваша заявка отправлена.Мы свяжемся с вами в ближайшее время.";
+        h4.textContent = "Спасибо!";
+        popUpMessage.style.display = "block";
       }
+
       postData(body)
         .then(response => {
           if (response.status !== 200) {
             throw new Error("status network not 200");
           }
-          popUpMessage.style.display = "block";
           statusMessage.textContent = "";
         })
         .catch(error => {
